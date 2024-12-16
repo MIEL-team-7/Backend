0458f64935d177426dc05992790425d716bf95de
AliakseiYafremau <116972232+AliakseiYafremau@users.noreply.github.com>
2024-12-16 11:17:59 +0100

    Revert "Correct authentication"

--- app/schemas/auth_schema.py
+++
@@ -1,8 +0,0 @@
-from pydantic import BaseModel
-
-
-class TokenSchema(BaseModel):
-    """Модель для сериализации токена"""
-
-    access_token: str
-    token_type: str
--- app/utils/routers.py
+++
@@ -1,7 +0,0 @@
-from app.routers.manager_router import manager_router
-from app.routers.auth_router import auth_router
-
-
-def register_routers(app):
-    app.include_router(manager_router)
-    app.include_router(auth_router)
